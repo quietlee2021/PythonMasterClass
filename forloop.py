c3a29f6c6b973f20fbdd16f85d3cea9cea32a6a8
Lee Greffin <lee.greffin@outlook.com>
2018-05-31 20:21:35 -0400

    Add files via upload

--- forloop.py
+++ forloop.py
@@ -11,6 +11,31 @@ print("The number is {0}".format(newNumber))
 for state in ["not pinin", "no more", "a stiff", "bereft of life"]:
     print("This parrot is " + state)
 
+for i in range(0,100,5):
+    print("i is {0} ".format(i))
+
+for i in range (1,13):
+    for j in range(1,13):
+        print("{1} times {0} is {2}" .format(i, j, i*j))
+    print("****************************************")
+
+for i in range (1,13):
+    for j in range(1,13):
+        print("{1} times {0} is {2}" .format(i, j, i*j), end='\t')
+    print(' ')
+    #print("****************************************")
+
+
+quote = "Alright, but apart from the Sanitation, the Medicine, Education, Wine, Public Order, Irrigation, Roads, the Fresh-Water System, and Public Health, what have the Romans ever done for us?/"
+
+# Use a for loop and an if statement to print just the capitals in the quote above.
+cleanedChar = ''
+for char in quote:
+    if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        cleanedChar = cleanedChar + char
+
+print("The capitals in the string are {0}".format(cleanedChar))
+
 quote = """
 Alright, but apart from the Sanitation, the Medicine, Education, Wine,
 Public Order, Irrigation, Roads, the Fresh-Water System,
@@ -18,9 +43,14 @@ and Public Health, what have the Romans ever done for us?
 """
 
 # Use a for loop and an if statement to print just the capitals in the quote above.
-cleanedChar = ''
 for char in quote:
     if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        cleanedChar = cleanedChar + quote
-newChar = cleanedChar
-print("The capitals in the string are {0}".format(newChar))
+        print(char, end='')
+print(" ")
+#Write a program to print out all the numbers from 0 to 100 that are divisible by 7
+for i in range(0,100,7):
+    print(i)
+#Alternate using a slice
+print("")
+for i in range(100)[::7]:
+    print(i)
