e6022261b7bd0c95dfd718cb590b955d31b34196
Lee Greffin <lee.greffin@outlook.com>
2018-06-05 19:43:31 -0400

    Add files via upload

--- continuebreak.py
+++ continuebreak.py
@@ -11,3 +11,24 @@ for i in range(0, 100, 7):
     if i > 0 and i%11 == 0:
         break
 
+print(" ")
+for i in range (0,20,1):
+    if (i ==0)or(i%3==0) or (i%5==0):
+        continue
+    print(i)
+
+#other solution 1
+print(" ")
+# using continue
+for x in range(20):
+    if x % 3 == 0 or x % 5 == 0:
+        continue
+    print(x)
+
+#other solution 2
+print(" ")
+# without continue
+for x in range(20):
+    if x % 3 != 0 and x % 5 != 0:
+        print(x)
+
